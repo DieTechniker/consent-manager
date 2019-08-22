97ccaaca5ca0ab16a26f42b392a0e1c360219c9b
Lorenz Kahl <p224162@tk.de>
2019-08-22 15:32:41 +0200

    Link zu den Zustimmungseinstellungen hinzugefügt

--- src/explicitConsent/script.js
+++ src/explicitConsent/script.js
@@ -3,12 +3,13 @@
 	  PLEASE CONTACT SUPPORT FOR ASSISTANCE IF YOU NEED TO MODIFY.
 ***************************************************************************/
 (function consent_prompt() {
-  var $el = document.getElementById('consent_prompt_submit'),
-    $modal = document.getElementById('__tealiumGDPRecModal'),
-    $closeBtn = $modal.getElementsByClassName('close_btn_thick')[0],
-    $privacy_pref_optin = document.getElementById('privacy_pref_optin'),
-    $privacy_pref_optout = document.getElementById('privacy_pref_optout'),
-    $documentElement = document.querySelector('html');
+  var $el = document.getElementById('consent_prompt_submit');
+  var $modal = document.getElementById('__tealiumGDPRecModal');
+  var $closeBtn = $modal.getElementsByClassName('close_btn_thick')[0];
+  var $privacy_pref_optin = document.getElementById('privacy_pref_optin');
+  var $privacy_pref_optout = document.getElementById('privacy_pref_optout');
+  var $documentElement = document.querySelector('html');
+  var $advancedOptions = document.getElementById('advanced_consent_options');
 
   $documentElement.classList.add('tealium-modal-open');
 
@@ -43,14 +44,25 @@
     $modal.style.display = 'none';
     $documentElement.classList.remove('tealium-modal-open');
   };
+  function advancedOptions() {
+    closePrompt();
+    utag.gdpr.showConsentPreferences();
+  }
   if (document.addEventListener) {
     $el.addEventListener('click', callBack, false);
     $closeBtn.addEventListener('click', closePrompt, false);
+    if ($advancedOptions) {
+      $advancedOptions.addEventListener('click', advancedOptions, false);
+    }
   } else if (document.attachEvent) {
     $el.attachEvent('click', callBack);
     $closeBtn.attachEvent('click', closePrompt);
+    if ($advancedOptions) {
+      $advancedOptions.attachEvent('click', advancedOptions);
+    }
   } else {
     $el.onclick = callBack;
     $closeBtn.onclick = closePrompt;
+    $advancedOptions.onclick = advancedOptions;
   }
 })();
