1fdd1219eb67d41ce2158f28586e9efbca3c29fc
Lorenz Kahl <p224162@tk.de>
2020-01-06 13:45:29 +0100

    added comment to the bootloader script

--- src/Bootloader/script.js
+++ src/Bootloader/script.js
@@ -4,7 +4,9 @@
         console.log('initializeConsentManager');
         // initialize the tealium consent manager - this will perform the following steps: 
         // * replace placeholders within the markup with custom texts defined in the tealium admin panel
-        // * mount the markup into the DOM 
+        // * mount the markup into the DOM
+        // * render the corresponding CSS-feature-classes on the <body>-element
+        // * write the corresponding JS-feature-map in window.tk.consentManager.features
         // * this will not(!) show any consent manager to the user
         utag.gdpr.showConsentPreferences();
     } else {
